e7d9e1616d44c41337b87649b7bdfc58014c8800
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2023-11-06 18:55:12 +0800

    make the image fit the body

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,12 +1,28 @@
 import React from "react";
 
 import NavBar from "../components/NavBar";
+import cw_hero from "../assets/CW-Hero.png";
 
 function LandingPage() {
   return (
-    <header>
-      <NavBar />
-    </header>
+    <>
+      <div
+        className="flex flex-col h-screen border-2 border-green-600"
+        id="home"
+      >
+        <header>
+          <NavBar />
+        </header>
+        <div className="border-8 border-blue-600 h-full">
+          <img src={cw_hero} alt="Picture" className="" />
+        </div>
+      </div>
+      <div className="flex flex-col h-screen border-2 border-red-600" id="home">
+        <div className="border-2 border-blue-600 ">
+          <img src={cw_hero} alt="Picture" className="" />
+        </div>
+      </div>
+    </>
   );
 }
 
