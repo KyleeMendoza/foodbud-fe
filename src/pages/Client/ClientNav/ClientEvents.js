49ca2629131178724bc088093867ea8e18b8e621
Kyle Dominic Mendoza <kylemendoza67@gmail.com>
2023-11-18 15:25:26 +0800

    catcher done

--- src/pages/Client/ClientNav/ClientEvents.js
+++ src/pages/Client/ClientNav/ClientEvents.js
@@ -15,33 +15,89 @@ function ClientEvents({ cookies }) {
         console.log(clientId);
         // console.log(response);
 
-        // Mapping the response to the state
-        const mappedEvents = response.events.map((eventData) => ({
-          eventID: eventData.eventID,
-          celebrant: eventData.celebrant,
-          event_date: eventData.event_date,
-          event_type: eventData.event_type,
-          package_type: eventData.package_type,
-        }));
+        // Checking if there are existing events
+        if (response.events && response.events.length > 0) {
+          // Mapping the response to the state
+          const mappedEvents = response.events.map((eventData) => ({
+            eventID: eventData.eventID,
+            celebrant: eventData.celebrant,
+            event_date: eventData.event_date,
+            event_type: eventData.event_type,
+            package_type: eventData.package_type,
+          }));
 
-        // Setting the state with the mapped events
-        setEvents(mappedEvents);
+          // Setting the state with the mapped events
+          setEvents(mappedEvents);
 
-        console.log(response.events);
+          console.log(response.events);
+        } else {
+          console.log("No events found for the given clientId.");
+          setEvents([]);
+          // You can handle the case when there are no events (e.g., display a message to the user).
+        }
       } catch (error) {
         console.error("Error:", error.message);
-        window.alert(
-          "An error occurred while fetching the bet history. Please try again later."
-        );
+        // window.alert(
+        //   "An error occurred while fetching the bet history. Please try again later."
+        // );
       }
     };
+
     fetchData();
   }, [clientId]);
 
   return (
     <div className=" h-full py-10 ">
       <div className="flex gap-5 ">
-        {events.map((events, key) => (
+        {events.length === 0 ? (
+          <p className="text-3xl text-white font-bold font-['Poppins']">
+            You have no events yet.
+          </p>
+        ) : (
+          <>
+            {events.map((events, key) => (
+              <NavLink
+                className="flex flex-col w-72 h-72 rounded-2xl drop-shadow-lg"
+                key={key}
+                to="/client/myEvent"
+                state={{ eventId: events.eventID }}
+              >
+                <div
+                  className={`font-['Poppins'] flex justify-start items-start flex-1 pt-5 px-5 ${
+                    key % 2 === 0 ? "bg-[#03A9F4]" : "bg-pink-500"
+                  } rounded-t-2xl`}
+                >
+                  <div
+                    className={`bg-white px-4 py-3 rounded-3xl ${
+                      key % 2 === 0 ? "text-[#03A9F4]" : "text-pink-500"
+                    }`}
+                  >
+                    <p className="text-xs capitalize font-semibold">
+                      {events.package_type}
+                    </p>
+                  </div>
+                </div>
+                <div className="flex flex-col justify-center gap-5 px-3 flex-1 bg-white rounded-b-2xl">
+                  <div className="flex flex-col gap-2">
+                    <p className="text-2xl font-bold capitalize">
+                      {events.celebrant}
+                    </p>
+                    <p className="text-sm capitalize">{events.event_type}l</p>
+                  </div>
+                  <div className="flex justify-between items-center">
+                    <p className="text-sm capitalize font-bold ">
+                      {events.event_date}
+                    </p>
+                    <Button variant="contained" size="small">
+                      Read more
+                    </Button>
+                  </div>
+                </div>
+              </NavLink>
+            ))}
+          </>
+        )}
+        {/* {events.map((events, key) => (
           <NavLink
             className="flex flex-col w-72 h-72 rounded-2xl drop-shadow-lg"
             key={key}
@@ -80,7 +136,7 @@ function ClientEvents({ cookies }) {
               </div>
             </div>
           </NavLink>
-        ))}
+        ))} */}
         {/* <div className="flex flex-col w-72 h-72 rounded-2xl">
           <div className="font-['Poppins'] flex justify-start items-start flex-1 pt-5 px-5 bg-pink-600 rounded-t-2xl">
             <div className="bg-pink-300 px-4 py-3 rounded-3xl">
