eb9f4a8ca993aa355c8478f10a00150f64ba8813
unknown <kylemendoza67@gmail.com>
2024-01-01 15:34:48 +0800

    packages bg hover complete

--- src/layouts/PackagesLayouts/PackagesHero.js
+++ src/layouts/PackagesLayouts/PackagesHero.js
@@ -19,8 +19,22 @@ function PackagesHero() {
   const [pic2, setPic2] = useState(false);
   const [pic3, setPic3] = useState(false);
 
+  const [bgImage, setBgImage] = useState(cs_aikp);
+
   return (
-    <div className={`h-fit monitor:h-screen flex flex-col justify-center`}>
+    <div className="h-fit monitor:h-screen flex flex-col justify-center">
+      {/* Background Image */}
+      <div
+        className="absolute inset-0 -z-50"
+        style={{
+          backgroundImage: `url(${bgImage})`,
+          backgroundSize: "cover",
+          backgroundRepeat: "no-repeat",
+          backgroundPosition: "center center",
+          opacity: 0.4, //Adjust this
+          transition: "background-image 0.5s ease-in-out",
+        }}
+      ></div>
       <div className="w-full h-full flex flex-col justify-center items-center">
         {/* Main Box Content */}
         <div className="flex flex-col border py-10 gap-6 w-full monitor:w-1/2">
@@ -52,8 +66,14 @@ function PackagesHero() {
             className={`border-2 px-7 py-4 w-fit font-tbc font-semibold text-title13 2xl:text-heading36 text-secondary400 rounded-xl hover:text-white hover:bg-secondary500 duration-200 ${
               aiphover ? `transform scale-110` : ""
             }`}
-            onMouseEnter={() => setAipHover(true)}
-            onMouseLeave={() => setAipHover(false)}
+            onMouseEnter={() => {
+              setAipHover(true);
+              setBgImage(cs_aikp);
+            }}
+            onMouseLeave={() => {
+              setAipHover(false);
+              setBgImage(cs_aikp);
+            }}
           >
             All-In Packages
           </a>
@@ -61,6 +81,12 @@ function PackagesHero() {
           <a
             href="#fullVenue"
             className="border-2 px-7 py-4 w-fit font-tbc font-semibold text-title13 2xl:text-heading36 text-secondary400 rounded-xl hover:text-white hover:bg-secondary500 duration-200 hover:transform hover:scale-110"
+            onMouseEnter={() => {
+              setBgImage(cw_fvs);
+            }}
+            onMouseLeave={() => {
+              setBgImage(cs_aikp);
+            }}
           >
             Full Venue Styling
           </a>
@@ -68,6 +94,12 @@ function PackagesHero() {
           <a
             href="#additional"
             className="border-2 px-7 py-4 w-fit font-tbc font-semibold text-title13 2xl:text-heading36 text-secondary400 rounded-xl hover:text-white hover:bg-secondary500 duration-200 hover:transform hover:scale-110"
+            onMouseEnter={() => {
+              setBgImage(cw_as);
+            }}
+            onMouseLeave={() => {
+              setBgImage(cs_aikp);
+            }}
           >
             Additional Services
           </a>
